1f47d6ab96ec93d875d70e790c37bc6126f06c00
Dat Nguyen <dnguyenk2@gmail.com>
2022-03-15 11:36:15 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(number){
+    return (Math.abs(number - 42));
+};
+
+function distanceFromHqInFeet(number){
+    return (distanceFromHqInBlocks(number) * 264);
+};
+
+function distanceTravelledInFeet(start, destination){
+    return (Math.abs(destination - start) * 264);
+};
+
+function calculatesFarePrice(start, destination){
+    var distance = distanceTravelledInFeet(start, destination);
+    var price = 0;
+    
+    if (distance < 400){
+        return 0;
+    }
+
+    if (distance >= 2500){
+        return ("cannot travel that far");
+    };
+
+    if (distance > 2000){
+
+        return 25;
+        
+    }else if ((distance > 400) && (distance < 2000)){
+
+        return ((distance - 400) * 0.02);
+
+    }
+};
+    
+
